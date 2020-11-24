68245bb75ea511ce353a9264fe001e5fa819c2a7
Izabela Garbacz <izabelagarbacz@vp.pl>
2020-11-24 16:11:13 +0100

    product

---
+++ src/main/java/pl/coderslab/entity/Product.java
@@ -0,0 +1,59 @@
+package pl.coderslab.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String capacity;
+
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String name, String capacity, double price) {
+        this.name = name;
+        this.capacity = capacity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
